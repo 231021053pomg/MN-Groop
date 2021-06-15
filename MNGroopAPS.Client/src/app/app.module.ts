aa1cc929ec41e92b9fba4b2cbcd29fd9f8a1186a
Fluxiaz <mgamads@gmail.com>
2021-06-15 09:28:39 +0200

    kategori added

--- MNGroopAPS.Client/src/app/app.module.ts
+++ MNGroopAPS.Client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BolcherComponent } from './bolcher/bolcher.component';
+import { KategoriComponent } from './kategori/kategori.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BolcherComponent
+    BolcherComponent,
+    KategoriComponent
   ],
   imports: [
     BrowserModule,
